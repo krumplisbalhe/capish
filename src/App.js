d2f4293f27c215b560271808e6388682158d2aab
rita.radovics <krumplisbalhe@gmail.com>
2019-10-28 22:25:50 +0100

    first commit, setting up grids

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="logo"></div>
+      <div className="buttonCreate"></div>
+      <div className="illustration"></div>
+      <div className="buttonJoin"></div>
+    </div>
+  )
+}
+
+export default App
